a3141fb936f145c3d5af36e4b641ef9e626af128
Pragya <pragyaprashar490@gmail.com>
2022-12-07 19:50:47 -0500

    added imports

--- src/questionnaire/profile/index.js
+++ src/questionnaire/profile/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./index.css"
 import {Link} from "react-router-dom";
-import {Route, Routes} from "react-router";
-import QuestionsAnswers from "../questionsanswers";
+
 
 const Profile =()=>{
     return(
