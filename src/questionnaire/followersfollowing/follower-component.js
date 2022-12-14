0030b2fbcdbbd65752bd851a4d3e6fa47471017d
Pragya <pragyaprashar490@gmail.com>
2022-12-14 02:48:30 -0500

    following, followers features added in profile and home components.

--- src/questionnaire/followersfollowing/follower-component.js
+++ src/questionnaire/followersfollowing/follower-component.js
@@ -1,52 +1,69 @@
 import React, {useEffect, useState} from "react";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {getUserFromId} from "../login/login-service";
 import {findPostsThunk} from "../../services/post-thunks";
+import {removeUserThunk, unfollowUserThunk} from "./following-followers-thunk";
 
-const FollowerComponent = ({userId})=>{
-    const [userObj, setUserObj] = useState(null);
-    let a = null;
+const FollowerComponent = ({userObj})=>{
+    //const [userObj, setUserObj] = useState(null);
+    const currentLoggedInUser = useSelector(s=>s.users.currentUser)
+    const dispatch=useDispatch();
 
-    useEffect(() => {
+    // let a = null;
+    //
+    // useEffect(() => {
+    //
+    //     const promiseUserObj = getUserFromId(userId);
+    //     promiseUserObj.then(result =>{
+    //                             console.log("result inside promise is ",result)
+    //                             setUserObj(result)
+    //                             a= result;
+    //                         }
+    //     );
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // },[a]);
 
-    const promiseUserObj = getUserFromId(userId);
-    promiseUserObj.then(result =>{
-                            console.log("result inside promise is ",result)
-                            setUserObj(result)
-                            a= result;
-                        }
-    );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-},[a]);
+    const setRemoveFollowHandler=()=>{
+        // setToggle(toggle=!toggle)
+        console.log("userObj in remove handler is ",userObj)
 
 
-    return(
-<>
-
-    {
-        userObj && userObj.username &&
-
-        <div className="row mt-2 border-bottom shadow-sm ">
-            <div className="col-2"><img alt="" src="../../../images/profile-pic-f.jpg"
-                                        className=" wd-profile-pic-f rounded-circle shadow mt-2"/>
-            </div>
-            <div className="col-7">
-                <div className="pt-1">
-                    <span className="fw-bold">{userObj.username}</span>
-                    {/*<p className="text-secondary">{str==="followers"?userObj.followers.length + " followers":userObj.following.length + " following"} </p>*/}
-                </div>
-            </div>
-            <div className="col-3 ">
-
-                <button className="w-50 wd-bttn text-light border-0 shadow rounded-pill bg-gradient mt-3 ">Follow</button>
-
-            </div>
-
-        </div>
+        const RemoveUserId=userObj._id
+        console.log("followUserId to be removed is ",RemoveUserId)
+        const obj = {
+            loggedInUserId: currentLoggedInUser._id,
+            anotherUserId : RemoveUserId
+        }
+        dispatch(removeUserThunk(obj))
     }
 
- </>
+    return(
+        <>
 
-     )
+            {
+
+
+                <div className="row mt-2 border-bottom shadow-sm ">
+                    <div className="col-2"><img alt="" src="../../../images/profile-pic-f.jpg"
+                                                className=" wd-profile-pic-f rounded-circle shadow mt-2 "/>
+                    </div>
+                    <div className="col-7">
+                        <div className="pt-1">
+                            <span className="fw-bold">{userObj.username}</span>
+                            {/*<p className="text-secondary">{str==="followers"?userObj.followers.length + " followers":userObj.following.length + " following"} </p>*/}
+                        </div>
+                    </div>
+                    <div className="col-3 ">
+
+                        <button className="w-50 wd-bttn wd-btn-color text-light border-0 shadow rounded-pill  mt-3 " onClick={setRemoveFollowHandler}>Remove</button>
+
+                    </div>
+
+                </div>
+            }
+
+        </>
+
+    )
 }
 export default  FollowerComponent;
